877179b1036513ed11d9d1018d0ada19df321044
JMBoulos <jeanmarc8boulos@gmail.com>
2025-04-22 20:57:32 +0300

    New components,installed prisma, added new routes and still coding in beta version -22/04/2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <Button>Click ME</Button>;
+  return <Hero />;
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,62 +1,86 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { HomeIcon, LogIn, LogInIcon, Sprout } from "lucide-react";
+import { HomeIcon, LogIn, LogInIcon, LogOut, Sprout } from "lucide-react";
 import ModeToggle from "./ModeToggle";
-import { StackServerApp } from "@stackframe/stack";
+import { stackServerApp } from "@/stack";
+import { UserButton } from "@stackframe/stack";
+import { getUserDetails } from "@/actions/user.actions";
 
+async function Navbar() {
+  const user = await stackServerApp.getUser();
+  const app = stackServerApp.urls;
+  const userProfile = await getUserDetails(user?.id);
 
-async function Navbar() => {
-
-const user = await StackServerApp.getUser();
-const app = StackServerApp.urls;
   return (
-  const user = stackServerApp.getUser();
-  <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
-    <div className="max-w-7xl mx-auto px-4">
-      <div className="flex items-center h-16 justify-between">
-        {/* Logo */}
-        <div className="flex items-center">
-          <Link
-            href="/"
-            className="text-xl font-bold text-primary font-mono tracking-wider"
-          >
-            Plantinventory
-          </Link>
-        </div>
-
-        {/* Navbar components */}
-
-        <div className="hidden md:flex items-center space-x-4">
-          <Button variant="ghost" className="flex items-center gap-2" asChild>
-            <Link href="/plants">
-              <Sprout className="w-4 h-4" />
-              <span className="hidden lg:inline">Plants</span>
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center h-16 justify-between">
+          {/*Logo */}
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-xl font-bold font-mono tracking-wider"
+            >
+              🌱 Plantventory
             </Link>
-          </Button>
+          </div>
 
-          <Button variant="ghost" className="flex items-center gap-2" asChild>
-            <Link href="/">
-              <Sprout className="w-4 h-4" />
-              <HomeIcon className="w-4 h-4" />
-              <span className="hidden lg:inline">Home</span>
-            </Link>
-          </Button>
+          {/*Navbar components*/}
 
-          <ModeToggle />
+          <div className="hidden md:flex items-center space-x-4">
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/plants">
+                <Sprout className="w-4 h-4" />
+                <span className="hidden lg:inline">Plants</span>
+              </Link>
+            </Button>
 
-          {/* Sign Button */}
-          <Button variant="ghost" className="flex items-center gap-2" asChild>
-            <Link href={app.signIn}>
-              <LogIn className="w-4 h-4" />
-              <LogInIcon className="w-4 h-4"/>
-              <span className="hidden lg:inline">Sign In</span>
-            </Link>
-          </Button>
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/">
+                <HomeIcon className="w-4 h-4" />
+                <span className="hidden lg:inline">Home</span>
+              </Link>
+            </Button>
+
+            <ModeToggle />
+
+            {user ? (
+              <>
+                {/*Sign out Button*/}
+                <Button
+                  variant="outline"
+                  className="flex items-center gap-2"
+                  asChild
+                >
+                  <Link href={app.signOut}>
+                    <LogOut className="w-4 h-4" />
+
+                    <span className="hidden lg:inline">Sign Out</span>
+                  </Link>
+                </Button>
+
+                <UserButton />
+              </>
+            ) : (
+              <>
+                {/*Sign Button*/}
+                <Button
+                  variant="ghost"
+                  className="flex items-center gap-2"
+                  asChild
+                >
+                  <Link href={app.signIn}>
+                    <LogIn className="w-4 h-4" />
+                    <span className="hidden lg:inline">Sign In</span>
+                  </Link>
+                </Button>
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
-};
+}
 
 export default Navbar;
